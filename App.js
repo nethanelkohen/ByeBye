64fd3caf89422e1011005384ba46d86ab82ec6b6
Martin Pham <martin.liem.pham@gmail.com>
2017-12-14 13:06:03 -0500

    pushing to pull from master

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from 'expo';
+import  Map  from './components/Map.js'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+      export default class App extends React.Component {
+        render() {
+          return (
+            
+              <Map /> 
+            
+          );
+        }
+      }
 
 const styles = StyleSheet.create({
   container: {
