6ec18d61c3bcd0aaa239d85ab2a532fb5c44afc0
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-01-27 22:19:13 -0500

    tnotes: TextMessage.js

--- components/TextMessage.js
+++ components/TextMessage.js
@@ -1,4 +1,6 @@
+// Import react and component from react library.
 import React, { Component } from 'react';
+// Import react native components.
 import {
   View,
   StyleSheet,
@@ -6,26 +8,34 @@ import {
   Text,
   AsyncStorage,
   Alert,
-  KeyboardAvoidingView,
-  Keyboard,
-  ScrollView
+  Keyboard
 } from 'react-native';
+// Import icon for ui display.
 import { Icon } from 'react-native-elements';
+// Import texts from messages components.
 import TextOne from './messages/TextOne.js';
 import TextTwo from './messages/TextTwo.js';
 import TextThree from './messages/TextThree.js';
 import TextFour from './messages/TextFour.js';
 import TextFive from './messages/TextFive.js';
+// Require json-circular-stringify to overrides JSON's
+// stringify to handle circular references for text message input.
 require('json-circular-stringify');
 
+// Create TextMessage component.
 class TextMessage extends Component {
+  // Create constructor and gain access to props and functions from parent.
   constructor(props) {
     super(props);
+    // Create state (message).
     this.state = {
       message: null
     };
   }
 
+  // saveMessage method saves user's message from TextInput to device's storage
+  // then navigates to map screen and dismisses keyboard from view. Alerts user if
+  // input is empty.
   saveMessage = () => {
     let message = this.state.message;
     if (!message) {
@@ -37,6 +47,8 @@ class TextMessage extends Component {
     }
   };
 
+  // oneText method sets user's text message choice to textOne using device's storage
+  // then navigates to map screen and dismisses keyboard from view.
   oneText = () => {
     let textOne = `I am alive. I got home.`;
     AsyncStorage.setItem('message', textOne);
@@ -44,6 +56,8 @@ class TextMessage extends Component {
     Keyboard.dismiss();
   };
 
+  // twoText method sets user's text message choice to textTwo using device's storage
+  // then navigates to map screen and dismisses keyboard from view.
   twoText = () => {
     let textTwo = `I made it home, but I can't keep living like this. I need to make changes.`;
     AsyncStorage.setItem('message', textTwo);
@@ -58,6 +72,8 @@ class TextMessage extends Component {
   //   Keyboard.dismiss();
   // };
 
+  // fourText method sets user's text message choice to textFour using device's storage
+  // then navigates to map screen and dismisses keyboard from view.
   fourText = () => {
     let textFour = `I made it home safe and sound. In your face.`;
     AsyncStorage.setItem('message', textFour);
@@ -65,6 +81,8 @@ class TextMessage extends Component {
     Keyboard.dismiss();
   };
 
+  // fiveText method sets user's text message choice to textFive using device's storage
+  // then navigates to map screen and dismisses keyboard from view.
   fiveText = () => {
     let textFive = `To all the haters who thought I'd never make it home, who thought my alcoholism was an issue, this one's for you: I am home safe and sound.`;
     AsyncStorage.setItem('message', textFive);
@@ -72,32 +90,41 @@ class TextMessage extends Component {
     Keyboard.dismiss();
   };
 
+  // Dismisses keyboard after user presses enter in TextInput.
   onKeyPress = ({ nativeEvent }) => {
     if (nativeEvent.key === 'Enter') {
       Keyboard.dismiss();
     }
   };
 
+  // Render React elements to device.
   render() {
+    // Pass in navigation props.
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.SaveMessageContainer}>
+        {/* Text input from react native component used to record what user types. */}
         <TextInput
           style={styles.MessageInput}
           multiline={true}
           placeholder="Enter Your Message"
           placeholderTextColor="black"
           returnKeyType="go"
+          // Sets text message (state) to whatever user types.
           onChangeText={text => this.setState({ message: text })}
           value={this.state.message}
+          // Tied to onKeyPress event which is used to dismiss keyboard.
           onKeyPress={this.onKeyPress}
         />
+        {/* Renders icons from react-native-elements for easier UI display.
+          Icons used to save user input or user text message choice. */}
         <Icon
           name="save"
           type="feather"
           color="#517fa4"
           raised={true}
           backgroundColor="white"
+          // Saves message on icon press.
           onPress={this.saveMessage}
         />
         <Text>Save Message</Text>
@@ -107,8 +134,10 @@ class TextMessage extends Component {
           color="#517fa4"
           raised={true}
           backgroundColor="white"
+          // Saves user text message to textOne on icon press.
           onPress={this.oneText}
         />
+        {/* Renders TextOne component */}
         <TextOne />
         <Icon
           name="arrow-right"
@@ -116,8 +145,10 @@ class TextMessage extends Component {
           color="#517fa4"
           raised={true}
           backgroundColor="white"
+          // Saves user text message to textTwo on icon press.
           onPress={this.twoText}
         />
+        {/* Renders TextTwo component */}
         <TextTwo />
         {/* <Icon
             name="arrow-right"
@@ -125,6 +156,7 @@ class TextMessage extends Component {
             color="#517fa4"
             raised={true}
             backgroundColor="white"
+            // Saves user text message to textThree on icon press.
             onPress={this.threeText}
           />
           <TextThree /> */}
@@ -134,8 +166,10 @@ class TextMessage extends Component {
           color="#517fa4"
           raised={true}
           backgroundColor="white"
+          // Saves user text message to textFour on icon press.
           onPress={this.fourText}
         />
+        {/* Renders TextFour component */}
         <TextFour />
         <Icon
           name="arrow-right"
@@ -143,14 +177,17 @@ class TextMessage extends Component {
           color="#517fa4"
           raised={true}
           backgroundColor="white"
+          // Saves user text message to textFive on icon press.
           onPress={this.fiveText}
         />
+        {/* Renders TextFive component */}
         <TextFive />
       </View>
     );
   }
 }
 
+// React Native styles.
 const styles = StyleSheet.create({
   SaveMessageContainer: {
     flex: 1,
@@ -178,4 +215,5 @@ const styles = StyleSheet.create({
   }
 });
 
+// Export TextMessage component.
 export default TextMessage;
