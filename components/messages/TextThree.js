550f21ef66e39759cd667a092d75b0bfe47a1ba9
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-02 19:25:06 -0500

    notes for textthree

--- components/messages/TextThree.js
+++ components/messages/TextThree.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+// Stateless functional component that creates text message for later use
+// in TextMessage.js
+
 const TextThree = () => <Text>I'M HOME</Text>;
 export default TextThree;
