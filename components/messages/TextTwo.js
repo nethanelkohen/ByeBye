bef7c170006959fd75c8fa75aeacd060c7064267
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-02 19:25:38 -0500

    notes for texttwo

--- components/messages/TextTwo.js
+++ components/messages/TextTwo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+// Stateless functional component that creates text message for later use
+// in TextMessage.js
+
 const TextTwo = () => (
   <Text>
     I made it home, but I can't keep living like this. I need to make changes.
