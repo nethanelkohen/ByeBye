3f6ac88d171cef81c6050ae32b19f8e8f7dd083e
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-02 19:24:03 -0500

    notes for textfive

--- components/messages/TextFive.js
+++ components/messages/TextFive.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+// Stateless functional component that creates text message for later use
+// in TextMessage.js
+
 const TextFive = () => (
   <Text>
     To all the haters who thought I'd never make it home, who thought my
