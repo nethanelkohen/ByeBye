96443bb0d825ad4b3ffe354242b540c427503048
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-02 19:24:45 -0500

    notes for textfour

--- components/messages/TextFour.js
+++ components/messages/TextFour.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+// Stateless functional component that creates text message for later use
+// in TextMessage.js
+
 const TextFour = () => (
   <Text>I made it home safe and sound. In your face.</Text>
 );
