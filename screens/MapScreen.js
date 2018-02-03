c26d17f4080472e524859acee733d5df3e8d436a
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-02 20:30:45 -0500

    notes: MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,22 @@
+// Import react and component from react library.
 import React, { Component } from 'react';
+// Import react native components.
 import { View, StyleSheet } from 'react-native';
+// Import Map component.
 import Map from '../components/Map.js';
 
+// Create MapScreen component.
 class MapScreen extends Component {
+  // Create constructor and gain access to props and functions from parent.
   constructor(props) {
     super(props);
   }
 
+  // Render React elements to device.
   render() {
     return (
       <View style={styles.container}>
+        {/* Renders Map component. */}
         <Map style={styles.map} />
       </View>
     );
